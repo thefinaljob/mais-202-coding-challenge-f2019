6b7e3143c756b9466f8bf9aef4a5b70da1d10d31
Nikolai Peram <nikolai.peram@gmail.com>
2019-09-10 22:12:34 -0400

    submission

---
+++ submission.py
@@ -0,0 +1,58 @@
+'''Am I a data scientist now? '''
+import sys, csv ,operator
+import matplotlib.pyplot as plt
+
+mortgage = 0.0
+rent = 0.0
+own = 0.0
+
+def do_everything():
+
+    home_data =csv.reader(open('home_ownership_data.csv', 'r'),delimiter=',')
+
+    #iterates through the home ownership data to find the loan amount for each ID
+    for first_row in home_data:
+
+        ident = str(first_row[0])
+        loan_type = str(first_row[1])
+        if loan_type == "home_ownership":
+            continue
+        look_loan(ident, loan_type)
+    
+    #Setting parameters of the graph here
+    names = ['MORTGAGE', 'OWN', 'RENT']
+    values = [mortgage, own, rent]
+    plt.bar(names, values) 
+    plt.xlabel('Home ownership')
+    plt.ylabel('Average loan amount ($)')
+    plt.title('Average loan amounts per home ownership')  
+    plt.autoscale()
+    plt.savefig('foo.png', bbox_inches='tight')
+
+def look_loan(ident, loan_type):
+
+    '''apparently python doesn't allow me to reset iterators,
+    who would have known? This function is here to make up for it'''
+
+    loan_data = csv.reader(open('loan_data.csv', 'r'),delimiter=',')
+
+    #conducts linear search of the loan data to see where the corresponding ID and loan
+    #amounts are found
+    for second_row in loan_data:
+
+            check_ident = str(second_row[0])
+
+            if ident == check_ident:
+                amt = float(second_row[1])
+                if loan_type == 'MORTGAGE':
+                    global mortgage 
+                    mortgage += amt
+                elif loan_type == 'RENT':
+                    global rent 
+                    rent += amt
+                elif loan_type == 'OWN':
+                    global own 
+                    own += amt
+                return
+
+do_everything()
